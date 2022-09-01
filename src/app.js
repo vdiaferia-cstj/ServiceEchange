79c44b46a6de2cd8e3af45538e8c2d0ef1d30804
Vincent Diaferia <1967480@cstj.qc.ca>
2022-09-01 15:00:32 -0400

    Fin du cours 1 sept

--- src/app.js
+++ src/app.js
@@ -1,12 +1,52 @@
 import express from 'express';
-
+import dayjs from 'dayjs';
 const app = express();
 
 //TODO: Ajouter du code ici
-app.get('/premiere',(req,res)=>{
+
+app.get('/deuxieme',(req,res)=>{
     res.status(200)
     res.set('Content-Type','text/plain');
     res.send('Première route avec express');
+});
+
+app.get('/date', (req,res) => {
+    res.status(200);
+    res.set('Content-Type','text/plain');
+    const today = dayjs();
+    res.send(today.format());
+});
+
+app.get('/maths/:/operation', (req,res) => {
+    const a = parseInt(req.query.a, 10);
+    const b =parseInt(req.query.b, 10);
+    let somme =0;
+
+
+    const operation = req.params.operation;
+
+switch (operation) {
+    case 'addition':
+        somme = a + b;
+    break;
+
+    case 'soustraction':
+        somme = a - b;
+    break;
+
+    case 'multiplication':
+        somme = a * b;
+        break;
+    case 'division':
+        somme = a / b ;
+    break;
+}
+
+     
+    res.status(200);
+    res.set('Content-Type', 'text/plain');
+    res.send(somme.toString);
+    
 });
 
 export default app;
