b1bf6c3ea786f6a3b034614f8f5e4c03d28710cd
Vincent Diaferia <1967480@cstj.qc.ca>
2022-09-01 13:38:17 -0400

    Fin du cours 30 août

---
+++ server.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+
+import app from './src/app.js';
+
+const PORT = 4200;
+
+app.listen(PORT, err => {
+
+    if(err) {
+        //Nous avons une erreur, on pourrait faire du code plus brillant.
+    process.exit(1);
+    }
+
+
+
+    console.log(chalk.blue('Server listenig on port ${PORT}'));
+});
+
+console.log(chalk.red.bgBlueBright('Test'));
